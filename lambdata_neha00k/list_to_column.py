5a14275ff083340e05849428beae7a79d252155f
Neha-kumari31 <neha.silicon@gmail.com>
2020-01-20 03:46:52 -0500

    added another utility function

---
+++ lambdata_neha00k/list_to_column.py
@@ -0,0 +1,13 @@
+#function to take a list, turn it into a series and add it to a dataframe as a new column
+import pandas as pd
+def list_to_column(df, mylist):
+    """
+    """
+    new_col = pd.Series(mylist)
+    df['new_col'] = new_col.values
+    return df
+
+
+df = pd.DataFrame({"A":[1,7,45, 9,10], "B": [5, 7, 4, 9, 70]})
+list_to_column(df,[9,56, 9,45, 6] )
+print(df.head())
